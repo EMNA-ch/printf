e04331cfaea4f910079981cf1702ce5e9949779d
farah <farah.lajimi1@gmail.com>
2023-08-22 09:38:35 -0700

    _strlen.c

--- _strlen.c
+++ _strlen.c
@@ -1,8 +1,3 @@
-
-
-
-
-
 /**
  * _strlen - length of a string
  * @s: the string
@@ -11,9 +6,8 @@
 
 int _strlen(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (0);
+int i;
+for (i = 0; s[i] != '\0'; i++)
+;
+return (0);
 }
