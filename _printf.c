35a03d9c854196e3b815d0e7e2361127606e13a9
EMNA-ch <cheibi.emna90@gmail.com>
2023-08-22 06:29:23 -0700

    commit task 0

--- _printf.c
+++ _printf.c
@@ -4,13 +4,12 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format
+ * @format: format.
  * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_char = 0;
+	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -20,14 +19,14 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0', i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/*write(1, &format[i], 1);*/
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -35,16 +34,17 @@ int _printf(const char *format, ...)
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
-		 	precision = get_precision(format, &i, list);
-		 	size = get_size(format, &i);
-		 	i++;
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
 			printed = handle_print(format, &i, list, buffer,
-				 flags, width, precision, size);
-		 	if (printed == -1)
-			 	return (-1);
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
 			printed_chars += printed;
 		}
 	}
+
 	print_buffer(buffer, &buff_ind);
 
 	va_end(list);
@@ -53,9 +53,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the content of the buffer if it exists.
- * @buffer: array of chars.
- * @buff_ind: index of whitch to add next char, represents the length.
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
